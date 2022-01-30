b2fa3019a7008878a0bc19343819dec91973e2e0
Ana Raquel <anaraquelpmatos@gmail.com>
2022-01-30 13:20:41 -0300

    Implementação do button de fechar pedido

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let prato = "";
+let bebida = "";
+let sobremesa = "";
+
+/*Seleção do prato escolhido*/
 function selecionarMenu(classeBotao) {
     const selecionado = document.querySelector(".selecionado");
 
@@ -6,9 +11,21 @@ function selecionarMenu(classeBotao) {
     }
     const botaoMenu = document.querySelector("." + classeBotao);
     botaoMenu.classList.add("selecionado");
-    prato = selecionarMenu(classeBotao);
 
+    prato = classeBotao;
+
+    /*Seleção das três opções para alterar botão de fechar pedido automaticamente*/
+    if ((prato !== "") && (bebida !== "") && (sobremesa !== "")) {
+
+        const elemento = document.querySelector("#envio__pedido");
+        elemento.innerHTML = "Fechar pedido";
+        document.getElementById("envio__pedido").style.backgroundColor = "rgba(50, 183, 47, 1";
+        document.getElementById("envio__pedido").style.borderColor = "rgba(50, 183, 47, 1";
+        document.getElementById("envio__pedido").style.fontWeight = "700";
+    }
 }
+
+/*Seleção da bebida escolhida*/
 function selecionarBebida(classeBotao2) {
     const selecionado2 = document.querySelector(".selecionado2");
 
@@ -18,7 +35,20 @@ function selecionarBebida(classeBotao2) {
     const botaoMenu = document.querySelector("." + classeBotao2);
     botaoMenu.classList.add("selecionado2");
 
+    bebida = classeBotao2;
+
+    /*Seleção das três opções para alterar botão de fechar pedido automaticamente*/
+    if ((prato !== "") && (bebida !== "") && (sobremesa !== "")) {
+
+        const elemento = document.querySelector("#envio__pedido");
+        elemento.innerHTML = "Fechar pedido";
+        document.getElementById("envio__pedido").style.backgroundColor = "rgba(50, 183, 47, 1";
+        document.getElementById("envio__pedido").style.borderColor = "rgba(50, 183, 47, 1";
+        document.getElementById("envio__pedido").style.fontWeight = "700";
+    }
 }
+
+/*Seleção da sobremesa escolhida*/
 function selecionarSobremesa(classeBotao3) {
     const selecionado3 = document.querySelector(".selecionado3");
 
@@ -27,4 +57,20 @@ function selecionarSobremesa(classeBotao3) {
     }
     const botaoMenu = document.querySelector("." + classeBotao3);
     botaoMenu.classList.add("selecionado3");
+
+    sobremesa = classeBotao3;
+
+    /*Seleção das três opções para alterar botão de fechar pedido automaticamente*/
+    if ((prato !== "") && (bebida !== "") && (sobremesa !== "")) {
+
+        const elemento = document.querySelector("#envio__pedido");
+        elemento.innerHTML = "Fechar pedido";
+        document.getElementById("envio__pedido").style.backgroundColor = "rgba(50, 183, 47, 1";
+        document.getElementById("envio__pedido").style.borderColor = "rgba(50, 183, 47, 1";
+        document.getElementById("envio__pedido").style.fontWeight = "700";
+    }
+    function finalizarPedido() {
+
+    }
+
 }
